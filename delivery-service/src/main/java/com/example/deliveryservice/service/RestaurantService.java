aebbadf11e81abdc5ed1e6710fd4ac245465db6a
samyogacharya12 <samyog.acharya@gmail.com>
2023-06-22 10:00:06 +0545

    addded load balancing in microservice

--- delivery-service/src/main/java/com/example/deliveryservice/service/RestaurantService.java
+++ delivery-service/src/main/java/com/example/deliveryservice/service/RestaurantService.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "restaurant-app",url = "localhost:8080/restaurant")
+@FeignClient(name = "RESTAURANT-APP")
 public interface RestaurantService {
 
-    @GetMapping("/orders/{orderId}")
+    @GetMapping("/restaurant/orders/{orderId}")
     OrderResponseDto findByOrderId(@RequestHeader(HttpHeaders.AUTHORIZATION) String token,
                          @PathVariable(value = "orderId") String orderId);
 }
